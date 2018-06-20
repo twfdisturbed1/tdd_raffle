2f2b5ba9b2087bc8932c3bd72c85d1cc4fbd04f1
William Mullins <twfdisturbed@hotmail.com>
2018-06-20 16:08:43 -0400

    first commit

---
+++ test_raffle.rb
@@ -0,0 +1,10 @@
+require "minitest/autorun"
+require_relative "tdd_raffle.rb"
+
+class TestYourFunctions < Minitest::Test
+
+    def test_assert_that_1_equals_1
+        assert_equal(1, 1)
+    end
+
+end
